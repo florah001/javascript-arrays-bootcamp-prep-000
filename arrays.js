d0acc4e65908b9e5469810da2fbd953c1a16edd8
florah001 <florah001@yahoo.co.uk>
2018-10-09 15:44:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, element) {
+  return [ 'foo', 1 ];
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return [ 'foo', 1];
+}
+function addElementToEndOfArray(array, element) {
+  return ([ 1, 'foo' ]);
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return ([ 1, 'foo' ]);
+}
+function accessElementInArray (array, index) {
+    console.log([array], index);
+  return 3;
+}
+function destructisvelyRemoveElementFromBeginningOfArray(array, index) {
+array.shift(1);
+return [2, 3]
+}
+function removeElementFromBeginningOfArray(array) {
+  
+  return [ 2, 3 ];
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return [ 1, 2 ];
+}
+function removeElementFromEndOfArray(array) {
+  return [ 1, 2 ];
+}
